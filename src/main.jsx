bdc5c83ba05ec3db9b01fcac3545b3b667777a9f
Kamasah Dickson <kamasahdickson19@gmail.com>
2022-12-16 04:07:23 +0000

    updated paths

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import ErrorPage from "./components/ErrorPage";
 import SingleCountry from "./components/SingleCountry";
-import Header from "./components/Header";
 
 import "./sass/sass-main.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route path="/" element={<App />}></Route>
 				<Route path="/:countriesID" element={<SingleCountry />}></Route>
 				<Route path="*" element={<ErrorPage />}></Route>
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 );
